389992d98cbf6be24895e0beee67def5bf2624cb
Marcelo <msr88957@gmail.com>
2024-10-15 21:55:51 -0300

    update

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
 
+    
     @Get()
     findAll() {
         return 'retornar todos os recados'
@@ -10,9 +11,26 @@ export class RecadosController {
 
     //@Get('fixo/:dinamico/:id')
     @Get(':id')
-    findOne(@Param('id') parametros: string) {
-        console.log(parametros)
-        return 'retorna um recado'
+    findOne(@Param('id') id: string) {
+        console.log(id)
+        return `retorna um recado ID ${id}`
+    }
+
+    // Pode ser adicionado o codigo de retorno, por padrão o post vai retornar o 201
+    //@HttpCode(201)
+    //@HttpCode(HttpStatus.OK)
+    @Post()
+    create(@Body() body: any) {
+        console.log(body)
+        return `Essa rota cria um recado`
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: any) {
+
+        console.log(id, body)
+        return 'atualiza o recado'
+
     }
 
 }
