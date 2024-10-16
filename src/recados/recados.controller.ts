fa08fc277ad832ab033cd7898b206517ec24058b
Marcelo <msr88957@gmail.com>
2024-10-16 12:25:12 -0300

    lint project

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,41 +1,46 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+  @Get()
+  findAll(@Query() params: any) {
+    console.log(params);
+    return 'retornar todos os recados';
+  }
 
-    
-    @Get()
-    findAll() {
-        return 'retornar todos os recados'
-    }
+  //@Get('fixo/:dinamico/:id')
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    console.log(id);
+    return `retorna um recado ID ${id}`;
+  }
 
-    //@Get('fixo/:dinamico/:id')
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        console.log(id)
-        return `retorna um recado ID ${id}`
-    }
+  // Pode ser adicionado o codigo de retorno, por padrão o post vai retornar o 201
+  //@HttpCode(201)
+  //@HttpCode(HttpStatus.OK)
+  @Post()
+  create(@Body() body: any) {
+    console.log(body);
+    return `Essa rota cria um recado`;
+  }
 
-    // Pode ser adicionado o codigo de retorno, por padrão o post vai retornar o 201
-    //@HttpCode(201)
-    //@HttpCode(HttpStatus.OK)
-    @Post()
-    create(@Body() body: any) {
-        console.log(body)
-        return `Essa rota cria um recado`
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    console.log(id, body);
+    return 'atualiza o recado';
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() body: any) {
-
-        console.log(id, body)
-        return 'atualiza o recado'
-
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return `deleta um recado ID ${id}`
-    }
-
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `deleta um recado ID ${id}`;
+  }
 }
