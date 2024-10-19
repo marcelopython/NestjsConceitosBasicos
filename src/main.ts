47d7352415f0091eb968907247825bc755c240ab
Marcelo <msr88957@gmail.com>
2024-10-18 21:49:14 -0300

    validation param

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove chaves que não estão no DTO
+      forbidNonWhitelisted: true, // Levantar erro quando a chave não existir
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
